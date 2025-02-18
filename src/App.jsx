b106b67784251af5b9ce0f1638779cb0b0f8bb57
Hafsa Rana <hafsamrana25@gmail.com>
2025-02-18 09:48:24 +0000

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./components/home-page";
+import ArticlesPage from "./components/articles-page/articles-page";
+import ArticlePage from "./components/article-page/article-page";
+import NavBar from "./components/nav-bar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/articles/:article_id" element={<ArticlePage />}></Route>
+        <Route path="/articles/:topic?" element={<ArticlesPage />}></Route>
+      </Routes>
+      <footer>
+        <p>Copyright Northcoders News App 2025</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
