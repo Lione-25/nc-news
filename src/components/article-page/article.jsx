d91c0b1544aab372d90fbc04061f3f93eb2fdccb
Hafsa Rana <hafsamrana25@gmail.com>
2025-02-20 12:05:40 +0000

    create article page

---
+++ src/components/article-page/article.jsx
@@ -0,0 +1,29 @@
+import { formatDateLong } from "../../utils";
+
+function Article({ article, showCommentsButton }) {
+  function scrollToComments() {
+    if (!showCommentsButton.current.disabled) {
+      showCommentsButton.current.focus();
+    }
+    showCommentsButton.current.scrollIntoView();
+  }
+  return (
+    <>
+      <img src={article.article_img_url} alt={article.title} />
+      <h1>{article.title}</h1>
+      <p className="article-topic">
+        Topic: <a href={`/articles/${article.topic}`}>{article.topic}</a>
+      </p>
+      <div className="article-info">
+        <p>
+          Posted on {formatDateLong(article.created_at)} by {article.author}
+        </p>
+        <button onClick={scrollToComments}>
+          {article.comment_count} comments
+        </button>
+      </div>
+      <p id="article-body">{article.body}</p>
+    </>
+  );
+}
+export default Article;
