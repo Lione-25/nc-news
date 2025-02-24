a09fc1b75da51511893ba6af8a0e0ea857371242
Hafsa Rana <hafsamrana25@gmail.com>
2025-02-24 18:16:10 +0000

    remove unnecessary comments

--- src/components/article-page/article-votes/vote-button.jsx
+++ src/components/article-page/article-votes/vote-button.jsx
@@ -16,33 +16,27 @@ function VoteButton({
     setIsError(false);
     setIsLoading(true);
     if (!hasBeenClicked) {
-      //updateLocalVotes();
       changeLocalVotes(action);
       setHasBeenClicked(true);
       patchArticleVotes(article_id, action)
-        //updateArticleVotes(article_id)
         .then(() => {
           setIsLoading(false);
         })
         .catch(() => {
           setIsError(true);
           changeLocalVotes(-action);
-          // reverseLocalVotes();
           setHasBeenClicked(false);
           setIsLoading(false);
         });
     } else {
-      //reverseLocalVotes();
       changeLocalVotes(-action);
       setHasBeenClicked(false);
       patchArticleVotes(article_id, -action)
-        //reverseArticleVotes(article_id)
         .then(() => {
           setIsLoading(false);
         })
         .catch(() => {
           setIsError(true);
-          //updateLocalVotes();
           changeLocalVotes(action);
           setHasBeenClicked(true);
           setIsLoading(false);
